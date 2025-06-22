b3561dbcf7dafccd81a443c4d752135ed597b228
AsadAlDenNairat <a.nueirat4@student.aaup.edu>
2025-06-22 15:02:38 +0300

    confdelall

--- script.js
+++ script.js
@@ -119,3 +119,10 @@ function deleteAllTodos() {
     }
     document.getElementById('deleteAllModal').style.display = 'flex';
 }
+function confirmDeleteAll() {
+    todos = [];
+    closeDeleteAllModal();
+    msgshow("All Tasks has been deleted.");
+    saveTodos();
+    render();
+}
