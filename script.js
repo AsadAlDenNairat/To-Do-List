f1f559805a999ddcbb01d2509f4cd990940bdc8d
AsadAlDenNairat <a.nueirat4@student.aaup.edu>
2025-06-22 14:49:48 +0300

    if1&2

--- script.js
+++ script.js
@@ -56,3 +56,11 @@ function add() {
     const todoInput = document.getElementById('newTodo');
     const errorMessage = document.getElementById('error-message');
     const taskText = todoInput.value.trim();
+    if (taskText === '') {
+        errorMessage.textContent = '⛔ Task cannot be empty';
+        return;
+    }
+    if (taskText.length < 5) {
+        errorMessage.textContent = '⛔ Task must be at least 5 characters long';
+        return;
+    }
